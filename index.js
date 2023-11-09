f433d3dad8f83c2c8d895a84a3212a380ae19233
sai6347 <saibharath7066@gmail.com>
2023-11-09 15:50:49 +0530

    updated post data

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ import express from "express";
 import mysql from "mysql";
 import cors from "cors";
 import multer from 'multer';
+import fs from "fs";
+import path from "path";
 
+const __dirname = path.resolve();
 const app =express()
 
 const storage = multer.memoryStorage(); 
@@ -19,6 +22,9 @@ app.use(express.json())
 
 app.use(cors())
 
+const employeeDocumentsDirectory = path.join(__dirname, "employee_documents");
+app.use("/employee_documents", express.static(employeeDocumentsDirectory));
+
 app.get("/", (req,res) => {
     res.json("Hello now you are connected to the backend....")
 })
@@ -31,8 +37,20 @@ app.get("/getEmp", (req,res) => {
     })
 })
 
+app.get("/getEmpDoc/:id", (req, res) => {
+    const empId = req.params.id;
+    const documentPath = path.join(employeeDocumentsDirectory, `employee_${empId}.pdf`);
+  
+    if (fs.existsSync(documentPath)) {
+      res.sendFile(documentPath);
+    } else {
+      res.status(404).send("Document not found");
+    }
+});
+
 app.post("/addEmp", upload.single('file'), (req,res) => {
-    const q = "insert into employee (`name`,`mobile`,`password`,`role`,`file`) values (?)"
+
+    const q = "insert into employee (`name`,`mobile`,`password`,`role`,`file`) values (?,?,?,?,?)"
     const values = [
         req.body.name,
         req.body.mobile,
@@ -40,11 +58,25 @@ app.post("/addEmp", upload.single('file'), (req,res) => {
         req.body.role,
         req.file.buffer
     ];
-    db.query(q,[values], (err,data) => {
-        if(err) return res.json(err)
-        return res.json("Registration done Successfully")
-    })
-})
+
+    db.query(q, values, (insertErr, insertResult) => {
+        if (insertErr) {
+          return res.status(500).json(insertErr);
+        }
+    
+        const employeeId = insertResult.insertId;
+        const documentPath = path.join(employeeDocumentsDirectory, `employee_${employeeId}.pdf`);
+
+
+        fs.writeFile(documentPath, req.file.buffer, (writeErr) => {
+          if (writeErr) {
+            return res.status(500).json(writeErr);
+          }
+    
+          return res.json("Registration and document upload done successfully");
+        });
+    });
+    });
 
 app.get("/getEmp/:id", (req, res) => {
     const empId = req.params.id;
